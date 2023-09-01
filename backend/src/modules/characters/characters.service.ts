c478d3ad546115c6d5d0c40cb94b383faa10d45c
Pablo Torrado <torradopablo1@gmail.com>
2023-09-01 20:15:11 -0300

    Add validate output service

--- backend/src/modules/characters/characters.service.ts
+++ backend/src/modules/characters/characters.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
 import axios from 'axios';
 import { PointerCharacters } from './interfaces/pointer-characters.interface';
 import { stringify } from 'querystring';
+import { PointerCharactersModel } from './models/pointer-characters.mode';
 
 @Injectable()
 export class CharacterService {
@@ -11,6 +14,8 @@ export class CharacterService {
       const queryParams = stringify(Object(query));
       const urlWithQuery = `${baseURL}?${queryParams}`;
       const { data } = await axios.get(urlWithQuery);
+      const myData = plainToClass(PointerCharactersModel, data);
+      await validate(myData, { skipMissingProperties: true });
       return data;
     } catch (error) {
       throw error;
